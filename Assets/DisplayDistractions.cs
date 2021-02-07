22529cea527c6060b8432906a321228c58c78ef7
Reaper <rjs5785@hotmail.com>
2021-02-07 01:03:17 -0500

    distraction appears, but doesn't change spots

--- Assets/DisplayDistractions.cs
+++ Assets/DisplayDistractions.cs
@@ -8,15 +8,40 @@ public class DisplayDistractions : MonoBehaviour
     [SerializeField]
     List<GameObject> distractionSpots;
 
+    float distractionDelay = 5.0f;
+    float showTimer = 0.0f;
+
+    int distractionSpot;
+
     // Start is called before the first frame update
     void Start()
     {
-        for (int i = 0; i < distractionSpots.Count; ++i)
-            distractionSpots[i].GetComponent<TextMeshProUGUI>().text = "distraction!";
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (distractionDelay <= 0.0f && showTimer <= 0.0f)
+        {
+            showTimer = 7.0f;
+            distractionSpot = Random.Range(0, distractionSpots.Count);
+            Debug.Log(distractionSpots.Count);
+        }
+        else
+        {
+            
+            distractionDelay -= Time.deltaTime;
+        }
+
+        if (showTimer <= 0.0f && distractionDelay <= 0.0f)
+        {
+            distractionSpots[distractionSpot].GetComponent<TextMeshProUGUI>().text = "";
+            distractionDelay = 5.0f;
+        }
+        else
+        {
+            distractionSpots[distractionSpot].GetComponent<TextMeshProUGUI>().text = "distraction!";
+            showTimer -= Time.deltaTime;
+        }
     }
 }
