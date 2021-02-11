3cb557636adc114ea66de91e0aed072130fb2294
Reaper <rjs5785@hotmail.com>
2021-02-11 12:01:36 -0500

    removed the jump button and now allow the player to tap anywhere on the screen to jump

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -14,6 +14,21 @@ public class Player : MonoBehaviour
         bc2d = GetComponent<BoxCollider2D>();
     }
 
+    void Update()
+    {
+        foreach (Touch touch in Input.touches)
+        {
+            if (touch.fingerId.Equals(0))
+            {
+                if (isGrounded())
+                {
+                    float jumpVelocity = 15f;
+                    rb2d.velocity = Vector2.up * jumpVelocity;
+                }
+            }
+        }
+    }
+
     public bool isGrounded()
     {
         RaycastHit2D hit = Physics2D.BoxCast(bc2d.bounds.center, bc2d.bounds.size, 0f, Vector2.down, .1f, layermask);
